715875b8d6401aeeeed7db5f12d5bb7b2cf7c38b
Aaron <kaven22314567@gmail.com>
2022-07-01 15:13:07 +0800

    change header an identify .php and require it in index.php

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let subNavContainer = document.querySelectorAll(".sub_nav_item_container");
 let mainNavItems = document.querySelectorAll(".main_nav_item");
 let subNavItems = document.querySelectorAll(".sub_nav_item_container li");
 console.log(subNavItems);
+// navbar animation ------------------------------------------------------------
 for (let i = 0; i < mainNavItems.length; i++) {
 	mainNavItems[i].addEventListener("click", function () {
 		for (let j = 0; j < subNavContainer.length; j++) {
@@ -20,3 +21,13 @@ for (let i = 0; i < mainNavItems.length; i++) {
 		navDropBtns[i].classList.toggle("nav_dropdown_active");
 	});
 }
+
+// navbar sub item click active class ------------------------------------------
+// for (let i = 0; i < subNavItems.length; i++) {
+// 	subNavItems[i].addEventListener("click", (e) => {
+// 		for (let j = 0; j < subNavItems; j++) {
+// 			subNavItems[j].classList.remove("sub_nav_item_active");
+// 		}
+// 		subNavItems[i].classList.add("sub_nav_item_active");
+// 	});
+// }
