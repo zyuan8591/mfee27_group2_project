5e757881129b828265eee8db5c87b1a071fe1381
Mike <s33165309000@gmail.com>
2022-07-02 00:14:33 +0800

    update js

--- js/app.js
+++ js/app.js
@@ -4,21 +4,18 @@ let mainNavItems = document.querySelectorAll(".main_nav_item");
 let subNavItems = document.querySelectorAll(".sub_nav_item_container li");
 // navbar animation ------------------------------------------------------------
 for (let i = 0; i < mainNavItems.length; i++) {
-	mainNavItems[i].addEventListener("click", function () {
-		for (let j = 0; j < subNavContainer.length; j++) {
-			if (
-				!subNavContainer[j].classList.contains("translateYtoNone") &&
-				i != j
-			) {
-				navDropBtns[j].classList.toggle("nav_dropdown_active");
-			}
-			if (i != j) {
-				subNavContainer[j].classList.add("translateYtoNone");
-			}
-		}
-		subNavContainer[i].classList.toggle("translateYtoNone");
-		navDropBtns[i].classList.toggle("nav_dropdown_active");
-	});
+    mainNavItems[i].addEventListener("click", function () {
+        for (let j = 0; j < subNavContainer.length; j++) {
+            if (!subNavContainer[j].classList.contains("translateYtoNone") && i != j) {
+                navDropBtns[j].classList.toggle("nav_dropdown_active");
+            }
+            if (i != j) {
+                subNavContainer[j].classList.add("translateYtoNone");
+            }
+        }
+        subNavContainer[i].classList.toggle("translateYtoNone");
+        navDropBtns[i].classList.toggle("nav_dropdown_active");
+    });
 }
 
 // navbar sub item click active class ------------------------------------------
@@ -34,12 +31,12 @@ for (let i = 0; i < mainNavItems.length; i++) {
 let filterItems = document.querySelectorAll(".filter-item");
 let filterDropDowm = document.querySelectorAll(".filter-dropdown");
 for (let i = 0; i < filterItems.length; i++) {
-	filterItems[i].addEventListener("mouseover", (e) => {
-		filterDropDowm[i].classList.remove("invisible");
-	});
-	filterItems[i].addEventListener("mouseout", (e) => {
-		filterDropDowm[i].classList.add("invisible");
-	});
+    filterItems[i].addEventListener("mouseover", (e) => {
+        filterDropDowm[i].classList.remove("invisible");
+    });
+    filterItems[i].addEventListener("mouseout", (e) => {
+        filterDropDowm[i].classList.add("invisible");
+    });
 }
 // add new material btn
 let newMaterialBtn = document.querySelector(".new-material-btn");
@@ -49,29 +46,44 @@ let materialContainer = document.querySelector(".material-container");
 console.log(newMaterialBtn);
 let i = 1;
 newMaterialBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	let row = document.createElement("div");
-	row.classList.add("row", "mb-3");
-	let col_8 = document.createElement("div");
-	col_8.classList.add("col-8");
-	let col_4 = document.createElement("div");
-	col_4.classList.add("col-4");
+    e.preventDefault();
+    let row = document.createElement("div");
+    row.classList.add("row", "mb-3");
+    let col_8 = document.createElement("div");
+    col_8.classList.add("col-8");
+    let col_4 = document.createElement("div");
+    col_4.classList.add("col-4");
 
-	let newMaterialName = materialName.cloneNode();
-	let newMaterialQ = materialQ.cloneNode();
-	if (i == 1) {
-		newMaterialName.name = `material-name-${i}`;
-		newMaterialQ.name = `material-q-${i}`;
-		i++;
-	} else {
-		newMaterialName.name = `material-name-${i}`;
-		newMaterialQ.name = `material-q-${i}`;
-		i++;
-	}
-	console.log(newMaterialQ);
-	col_8.appendChild(newMaterialName);
-	col_4.appendChild(newMaterialQ);
-	row.appendChild(col_8);
-	row.appendChild(col_4);
-	materialContainer.appendChild(row);
+    let newMaterialName = materialName.cloneNode();
+    let newMaterialQ = materialQ.cloneNode();
+    if (i == 1) {
+        newMaterialName.name = `material-name-${i}`;
+        newMaterialQ.name = `material-q-${i}`;
+        i++;
+    } else {
+        newMaterialName.name = `material-name-${i}`;
+        newMaterialQ.name = `material-q-${i}`;
+        i++;
+    }
+    console.log(newMaterialQ);
+    col_8.appendChild(newMaterialName);
+    col_4.appendChild(newMaterialQ);
+    row.appendChild(col_8);
+    row.appendChild(col_4);
+    materialContainer.appendChild(row);
+});
+
+let newRecipePage = documnet.querySelector(".new-recipe-page");
+let addRecipeBtn = document.querySelector(".add-recipe-btn");
+let backToRecipe = document.querySelector(".back-recipe");
+
+addRecipeBtn.addEventListener("click", function () {
+    e.preventDefault();
+	newRecipePage.classList.remove("invisible");
+    
+});
+backToRecipe.addEventListener("click", function () {
+    e.preventDefault();
+	newRecipePage.classList.add("invisible");
+    
 });
