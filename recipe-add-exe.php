bc6b0693bf5edc454192c84a2e36f1d994785e7a
aaron <kaven22314567@gmail.com>
2022-07-06 09:07:31 +0800

    update add page

--- recipe-add-exe.php
+++ recipe-add-exe.php
@@ -1,11 +1,15 @@
 <?php
+//add recipe
 $addRecipeName = isset($_GET["name"]) ? $_GET["name"] : "";
 $addCateFood = isset($_GET["category_food"]) ? $_GET["category_food"] : 1;
 $addCateProduct = isset($_GET["category_product"])
 	? $_GET["category_product"]
 	: 1;
+$addRecipeContent = isset($_GET["content"]) ? $_GET["content"] : "";
 echo "name: $addRecipeName <br> catfood: $addCateFood 
 <br> catProduct: $addCateProduct <br> ";
+
+//add material
 $i = 0;
 while (isset($_GET["material-name-$i"])) {
 	$material[$i] = $_GET["material-name-$i"];
