a250f16485cf89ddd0b7728c07b9aab2c80df4eb
NhOuOiB <s33165309000@gmail.com>
2022-07-07 17:13:49 +0800

    list unlist complete

--- list-unlist.php
+++ list-unlist.php
@@ -10,12 +10,5 @@ if($valid==1){
     $sql="UPDATE products SET valid=1 WHERE id=$id";
 }
 
-echo $sql;
-if ($conn->query($sql) === TRUE) {
-    echo "資料表 users 修改完成";
-} else {
-    echo "修改資料表錯誤: " . $conn->error;
-}
-
 $conn->close();
 ?>
