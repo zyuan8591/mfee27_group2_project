26f8d65efe39458b38503067028a9436b46cbc6f
NhOuOiB <s33165309000@gmail.com>
2022-07-08 10:51:59 +0800

    add complete

--- add-product-exe.php
+++ add-product-exe.php
@@ -1,27 +1,29 @@
 <?php
 require("./db-connect.php");
 
-if(!isset($_GET("name"))){
-    echo "請輸入欄位";
-}
+
 
 $name=$_GET["name"];
 $categoryMain=$_GET["category_main"];
-$categorySub-$_GET["category_sub"];
+$categorySub=$_GET["category_sub"];
 $price=$_GET["price"];
 $inventory=$_GET["inventory"];
 $intro=$_GET["intro"];
 $spec=$_GET["spec"];
 $now=date('Y-m-d H:i:s');
+$pic=$_GET["product_main_img"];
 
-
-$sql="INSERT INTO products (name, category_main, category_sub, price, inventory, intro, spec, now, valid) VALUES ('$name', '$category_main', '$category_sub', '$price', '$inventory', '$intro', '$spec', '$now', 1)";
+if(empty($name)){
+    echo "請輸入欄位";
+}
+$sql="INSERT INTO products (name, category_main, category_sub, price, inventory, intro, spec, product_main_img, create_time, valid) 
+VALUES ('$name', '$categoryMain', '$categorySub', '$price', '$inventory', '$intro', '$spec', '$pic', '$now', 1)";
 
 echo $sql;
 if ($conn->query($sql) === TRUE) {
     echo "新增完成";
+    echo $conn->insert_id;
 } else {
     echo "新增失敗" . $conn->error;
 }
 $conn->close();
-?>
